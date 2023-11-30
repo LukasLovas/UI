d1ad1f296c8f9a164b79bdcdb3cb7ca217932382
LukasLovas <xlovasl@stuba.sk>
2023-11-30 14:08:13 +0100

    start

--- Zadanie_3/main.py
+++ Zadanie_3/main.py
@@ -1,5 +1,9 @@
 import torch
+import numpy as np
+import pandas as pd
+import sklearn.datasets as datasets
 
 if __name__ == "__main__":
-    print(torch.rand(5, 3))
-
+    ds = datasets.load_iris()
+    data = pd.DataFrame(data=np.c_[ds['data'], ds['target']], columns=[ds["feature_names"] + ['target']])
+    print(data.to_markdown(index=False))
